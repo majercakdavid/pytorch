113ae07fb68a23ad13cdb90c4b453a8ac6172108
Han, Xu <xu.han@intel.com>
2023-08-17 08:36:05 +0000

    default turn off pch, and use for cpp_wrapper.

--- torch/_inductor/codecache.py
+++ torch/_inductor/codecache.py
@@ -1153,6 +1153,7 @@ class CppWrapperCodeCache:
                         extra_cflags=[extra_cflags],
                         extra_ldflags=[extra_ldflags],
                         extra_include_paths=[extra_include_paths],
+                        use_pch=True,
                     )
                     log.debug("Cpp wrapper done building %s", filepath)
                 else:
